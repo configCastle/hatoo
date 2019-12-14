325e94d87cc4b04db162b169a412df25d5056f42
Matvey Kuznetsov <mattewkuz@gmail.com>
2019-12-14 03:11:19 +0300

    sets-service started

--- src/app/sets-service/sets.service.ts
+++ src/app/sets-service/sets.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
+export interface ISet {
+  global: any;
+  services: any[];
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class SetsService {
-  currentUserSets$: Observable<any[]> = of([]);
-  getById$(): Observable<> {}
+  currentUserSets$: Observable<ISet[]> = of([]);
+  getById$(): Observable<ISet> {
+    return of()
+  }
   createSet$() { }
 }
