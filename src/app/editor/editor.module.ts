d63da2777bc1fa00f6686ca73c007a273780b99f
MatthewKuznetsov <mattewkuz@gmail.com>
2020-03-01 12:24:16 +0300

    graphic and text editors moved

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { EditorComponent } from './editor.component';
 import { UIModule } from '../ui.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { EditorElementComponent } from './graphic-editor/editor-form/editor-element/editor-element.component';
+import { EditorElementComponent } from './docker-compose/graphic-editor/editor-form/editor-element/editor-element.component';
 import { CommonModule } from '@angular/common';
 import { YAMLParserService } from '../Parser/yaml-parser.service';
 import { FormGroupParserService } from '../Parser/FormGroupParser/form-group-parser.service';
-import { EditorFormComponent } from './graphic-editor/editor-form/editor-form.component';
+import { EditorFormComponent } from './docker-compose/graphic-editor/editor-form/editor-form.component';
 import { MonacoEditorModule } from 'ngx-monaco-editor';
-import { TextEditorComponent } from './text-editor/text-editor.component';
+import { TextEditorComponent } from './docker-compose/text-editor/text-editor.component';
 import { EditorService } from './editor-service/editor.service';
-import { GraphicEditorComponent } from './graphic-editor/graphic-editor.component'
-import { TextEditorService } from './text-editor/text-editor.service';
-import { GraphicEditorService } from './graphic-editor/graphic-editor.service';
+import { GraphicEditorComponent } from './docker-compose/graphic-editor/graphic-editor.component'
+import { TextEditorService } from './docker-compose/text-editor/text-editor.service';
+import { GraphicEditorService } from './docker-compose/graphic-editor/graphic-editor.service';
 
 @NgModule({
   imports: [
