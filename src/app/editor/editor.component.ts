2d8c756c14408a9a495f2f51c65ceba311f3e89c
Кузнецов М.П <KuznecovMP@ugpa.ru>
2020-04-16 23:27:44 +0300

    monaco resize width

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { EditorService } from './editor.service';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -9,9 +9,16 @@ import { map } from 'rxjs/operators';
   styleUrls: ['editor.component.scss']
 })
 export class EditorComponent {
+  @HostListener('window:resize')
+  onResize() {
+    this.textEditorWidth = window.innerWidth * 0.4 - 10;
+  }
+
+  textEditorWidth: number;
   fileName$: Observable<string>;
 
   constructor(_editorService: EditorService) {
+    this.textEditorWidth = window.innerWidth * 0.4 - 10;
     this.fileName$ = _editorService.file$
       .pipe(map(s => s ? s.name : ''));
   }
